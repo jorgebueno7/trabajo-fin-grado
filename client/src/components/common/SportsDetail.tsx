033c6e65a7ea37dedcf2ae1b42fd3902ac414535
jorgebueno7 <jorge7bueno@gmail.com>
2024-11-11 19:07:39 +0100

    Añadido detalles del deporte

--- client/src/components/common/SportsDetail.tsx
+++ client/src/components/common/SportsDetail.tsx
@@ -32,13 +32,18 @@ const SportDetail = () => {
     }
 
     return (
-        <div>
-            <h1>{sport.nombre}</h1>
-            <p>{sport.descripcion}</p>
-            <p>{sport.informacion}</p>
-            <p>{sport.categoria}</p>
-            <p>{sport.equipamiento}</p>
-        </div>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href={`/sports/${sport.id_deporte}`}>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{sport.nombre}</h5>
+                </a>
+                <hr></hr><br></br>
+                <p className="text-gray-700 dark:text-gray-400">Descripción: <strong>{sport.descripcion}</strong></p>
+                <p className="text-gray-700 dark:text-gray-400">Información: <strong>{sport.informacion}</strong></p>
+                <p className="text-gray-700 dark:text-gray-400">Categoria: <strong>{sport.categoria}</strong></p>
+                <p className="text-gray-700 dark:text-gray-400">Equipamiento: <strong>{sport.equipamiento}</strong></p>
+            </div>
+        </div>    
     );
 };
 
