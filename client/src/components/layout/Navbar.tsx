b2536f70ce1602d76b22387c232d7b2bd69d0301
jorgebueno7 <jorge7bueno@gmail.com>
2025-03-25 17:14:23 +0100

    Fix navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -44,8 +44,6 @@ const Navbar = () => {
             <NavLink to="/news" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Noticias</NavLink>
           </>
         )}
-        <NavLink to="/rankings" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Ranking</NavLink>
-        <NavLink to="/news" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Noticias</NavLink>
       </div>
       <div className="flex justify-end w-full space-x-4">
         {isLoggedIn ? (
