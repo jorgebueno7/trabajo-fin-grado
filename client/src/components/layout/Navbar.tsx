b836733fe5da2d00a341a57cc012df5c5214c309
jorgebueno7 <jorge7bueno@gmail.com>
2025-03-25 17:28:30 +0100

    Fix navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -38,12 +38,16 @@ const Navbar = () => {
         {/* {isLoggedIn && (
           <NavLink to="/my-events" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Mis eventos</NavLink>
         )} */}
-        {isAdmin && (
+        {isAdmin ? (
           <>
             <NavLink to="/rankings" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Ranking</NavLink>
             <NavLink to="/news" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Noticias</NavLink>
           </>
-        )}
+        ) : (
+        <>
+          <NavLink to="/rankings" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Ranking</NavLink>
+          <NavLink to="/news" className="font-bold px-3 py-2 text-slate-600 rounded-lg hover:bg-slate-100 hover:text-slate-900">Noticias</NavLink>
+        </>)}
       </div>
       <div className="flex justify-end w-full space-x-4">
         {isLoggedIn ? (
