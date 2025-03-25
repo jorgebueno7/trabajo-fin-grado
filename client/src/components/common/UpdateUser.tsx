73e59cfe804198a7f215f2419c1156f3362816b7
jorgebueno7 <jorge7bueno@gmail.com>
2025-03-25 17:07:21 +0100

    Botones

--- client/src/components/common/UpdateUser.tsx
+++ client/src/components/common/UpdateUser.tsx
@@ -158,7 +158,7 @@ const UpdateSport = () => {
                             <button 
                                 type="button" 
                                 onClick={handleNext} 
-                                className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                                className="w-full text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                             >
                                 <span>Continuar</span>
                             </button>
@@ -235,7 +235,7 @@ const UpdateSport = () => {
                             <button 
                                 type="button" 
                                 onClick={handlePrevious} 
-                                className="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                                className="w-full text-white bg-gray-400 hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
                             >
                                 <span>Retroceder</span>
 
