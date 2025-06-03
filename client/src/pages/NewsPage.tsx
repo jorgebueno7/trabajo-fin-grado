23372006d2c550d90a75bfba0f9db5d5424cb93b
jorgebueno7 <jorge7bueno@gmail.com>
2025-06-03 17:41:33 +0200

    Paginar noticias

--- client/src/pages/NewsPage.tsx
+++ client/src/pages/NewsPage.tsx
@@ -7,6 +7,8 @@ import UserContext from '../context/UsersContext';
 
 dayjs.extend(utc);
 
+const ITEMS_PER_PAGE = 6;
+
 const NewsPage = () => {
     interface News {
         id_noticia: number;
@@ -19,6 +21,20 @@ const NewsPage = () => {
 
     const [news, setNews] = useState<News[]>([]);
     const { isLoggedIn, user } = useContext(UserContext);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const nextPageEvents = () => {
+        if (endIndex < news.length) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const prevPageEvents = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
     const navigate = useNavigate();
 
     const navigateToCreateNew = () => {
@@ -34,40 +50,76 @@ const NewsPage = () => {
     }
     useEffect(() => {   
         fetchNews();
+        setCurrentPage(1);
     }, []);
+
+    const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+    const endIndex = startIndex + ITEMS_PER_PAGE;
+    const currentNews = news.slice(startIndex, endIndex);
 
     return (
         <div className="p-5">
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center flex-wrap mb-6">
                 <h1 className="text-2xl font-bold ml-2">Últimas noticias</h1>
-                {
-                isLoggedIn && (user?.role === 'organizador' || user?.role === 'administrador') && (
+                {isLoggedIn && (user?.role === 'organizador' || user?.role === 'administrador') && (
                     <button
                         onClick={navigateToCreateNew}
-                        className="inline-flex items-center px-3 py-2 ml-2 mt-6 mb-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600"
+                        className="inline-flex items-center px-3 py-2 ml-2 mt-4 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600"
                     >
                         Crear noticia
                     </button>
-                )
-                }
+                )}
             </div>
-
-            <ul>
-                {news.map((item) => (
-                    <li key={item.id_noticia} className="mb-4 p-4 border rounded shadow">
-                        <h2 className="text-xl font-semibold">{item.titulo}</h2>
-                        <p className="text-gray-600">{item.subtitulo}</p>
+      
+            <div className="grid grid-cols-3 gap-6">
+                {currentNews.map((item) => (
+                    <div
+                        key={item.id_noticia}
+                        className="bg-white rounded-xl shadow-lg overflow-hidden border border-gray-200 hover:shadow-lg transition"
+                    >
                         <img
                             src={`http://localhost:5000/sportly/news/${item.id_noticia}/imagen`}
                             alt={item.titulo}
-                            className="mt-2 w-full h-auto"
+                            className="w-full h-max object-cover"
                         />
-                        <p className="text-gray-500 mt-2">
-                            Fecha: {dayjs.utc(item.fecha_creacion).format('DD-MM-YYYY HH:mm:ss')}
-                        </p>
-                    </li>
+                        <div className="p-4">
+                            <h1 className="text-lg font-semibold text-gray-800">{item.titulo}</h1>
+                            <h3 className="text-gray-600 text-sm">{item.subtitulo}</h3>
+                            <p className="text-gray-500 text-xs mt-2">
+                                Publicado el: {dayjs.utc(item.fecha_creacion).format('DD-MM-YYYY HH:mm')}
+                            </p>
+                        </div>
+                    </div>
                 ))}
-            </ul>
+            </div>
+            <div className="flex justify-center mt-4 space-x-6">
+                <button
+                    onClick={prevPageEvents}
+                    disabled={currentPage === 1}
+                    className={`mt-3 text-blue-600 hover:underline ${
+                    currentPage === 1
+                        ? ' cursor-not-allowed'
+                        : ''
+                    }`}
+                >
+                    Anterior
+                </button>
+
+                <span className="text-gray-700 ml-3 mr-3 mt-3">{currentPage}</span>
+
+                <button
+                    onClick={nextPageEvents}
+                    disabled={endIndex >= news.length}
+                    className={`mt-3 text-blue-600 hover:underline ${
+                    endIndex >= news.length
+                        ? 'cursor-not-allowed'
+                        : ''
+                    }`}
+                >
+                    Siguiente
+                </button>
+            </div>
+
         </div>
     );
 };
