16771c806be35a27b6b7ddee7eff0d7d880ba07b
jorgebueno7 <jorge7bueno@gmail.com>
2025-07-02 18:18:50 +0200

    Fix botones listado de valoraciones para el administrador

--- client/src/components/RatingList.tsx
+++ client/src/components/RatingList.tsx
@@ -119,15 +119,16 @@ const Valoraciones = () => {
                                 <p><strong>Usuario:</strong> {rating.user.email}</p>
                                 <p><strong>Valoración:</strong> <span className="text-yellow-500">{renderStars(rating.valoracion)}</span></p>
                                 <p><strong>Comentario:</strong> {rating.comentario}</p>
-                                <button
-                                    className="mt-4 mr-4 w-50 text-white bg-blue-800 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium 
-                                        rounded-lg text-sm px-2.5 py-2.5 text-center dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-800" 
-                                    onClick={() => navigateToUpdateRating(rating.id_rating)}>Modificar valoración</button>
-                                <button
-                                    className="w-50 text-white bg-red-800 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium 
-                                        rounded-lg text-sm px-2.5 py-2.5 text-center dark:bg-red-700 dark:hover:bg-red-800 dark:focus:ring-red-800" 
-                                    onClick={() => handleDeleteRating(rating.id_rating)}>Eliminar valoración</button>
-
+                                <div className="flex gap-4 mt-2">
+                                    <button
+                                        className="w-1/2 text-white bg-blue-800 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium 
+                                            rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-800"
+                                        onClick={() => navigateToUpdateRating(rating.id_rating)}>Modificar valoración</button>
+                                    <button
+                                        className="w-1/2 text-white bg-red-800 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium 
+                                            rounded-lg text-sm px-4 py-2 text-center dark:bg-red-700 dark:hover:bg-red-800 dark:focus:ring-red-800" 
+                                        onClick={() => handleDeleteRating(rating.id_rating)}>Eliminar valoración</button>
+                                </div>
                             </div>
                         )) 
                         : 
